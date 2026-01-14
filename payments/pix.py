671c4db2edd655874863104a1f6214a04003b3e2
Kevin Romanin <kevinromanin145@gmail.com>
2026-01-14 12:35:12 -0300

    finaliza projeto

---
+++ payments/pix.py
@@ -0,0 +1,22 @@
+import uuid
+import qrcode
+
+class Pix:
+    def __init__(self):
+        pass
+
+    def create_payment(self, base_dir=""):
+        # criar o pagamento na instituicao financeira
+        bank_payment_id = str(uuid.uuid4())
+
+        # codigo_copia_e_cola_123
+        hash_payment = f'hash_payment_{bank_payment_id}'
+
+        # qr code
+        img = qrcode.make(hash_payment)
+
+        # salvar a imagem como arquivo PNG
+        img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png") # type: ignore
+
+        return {"bank_payment_id": bank_payment_id,
+                "qr_code_path": f"qr_code_payment_{bank_payment_id}"}
